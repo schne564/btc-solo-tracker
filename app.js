ffef143ac068deff062f10e72798f0910ddfc75f
schne564 <Schne564@gmail.com>
2025-08-09 22:44:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Replace WORKER_URL with your Cloudflare Worker URL after deployment, e.g.
 // const WORKER_URL = 'https://your-worker.example.workers.dev';
-const WORKER_URL = "https://broad-cell-151e.schne564.workers.dev/";
+const WORKER_URL = "https://broad-cell-151e.schne564.workers.dv/";
 const defaultAddr = "bc1qd6mfkav3yzztuhpq6qg0kfm5fc2ay7jvy52rdn";
 let addr = defaultAddr;
 const refreshInterval = 60000; // 60s
